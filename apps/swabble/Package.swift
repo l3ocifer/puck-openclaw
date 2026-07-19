54e14632e9a77af112dfe58dcfb8d9ee3a3ffe8d
null <lpask001@gmail.com>
2026-07-19 01:38:59 +0000

    fix(deps): update all non-major dependencies

--- apps/swabble/Package.swift
+++ apps/swabble/Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         .executable(name: "swabble", targets: ["SwabbleCLI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/steipete/Commander.git", exact: "0.2.3"),
-        .package(url: "https://github.com/apple/swift-testing", from: "6.3.1"),
+        .package(url: "https://github.com/steipete/Commander.git", exact: "0.2.4"),
+        .package(url: "https://github.com/apple/swift-testing", from: "6.3.2"),
     ],
     targets: [
         .target(
